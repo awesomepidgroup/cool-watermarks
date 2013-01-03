cf516f3fb81e56dddcd8c456439a3c1695f4ed16
gabriel <chocolateblancowilliams@gmail.com>
2013-01-03 13:34:29 +0100

    Creado el paquete util, que incluye las nuevas clases Log.java y Strings.java

---
+++ src/util/Log.java
@@ -0,0 +1,13 @@
+package util;
+
+/**
+	Clase Log, usada para la monitorización
+	del flujo del código.
+*/
+
+public static class Log {
+	
+	public static void print(String systemStatus) {
+		System.out.println(systemStatus);
+	}
+}
---
+++ src/util/Strings.java
@@ -0,0 +1,31 @@
+package util;
+
+/**
+	Clase Strings que contiene las explicaciones
+	de los pasos realizados para la obtención de
+	la nueva imagen.
+*/
+
+public static class Strings {
+
+	public static String getExplanation(int index) {
+		
+		String result = "";
+
+		if(index == 1) {
+			result = "Texto para paso 1";
+		} else if(index == 2) {
+			result = "Texto para paso 2";
+		} else if(index == 3) {
+			result = "Texto para paso 3";
+		} else if(index == 4) {
+			result = "Texto para paso 4";
+		} else if(index == 5) {
+			result = "Texto para paso 5";
+		} else {
+			result = "Texto para paso 6";
+		}
+
+		return result;
+	}
+}
